5841594b6eb42430bab682377ac371838d9fa574
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2023-02-06 22:29:16 +0100

    Added data objects

--- AuthApp/api/models.py
+++ AuthApp/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+class Rating(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user','post'),)
+        index_together = (('user','post'),)
